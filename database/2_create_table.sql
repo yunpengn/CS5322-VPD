4340b1d4ab3a612a81ac0a6bc051b921ea1a8298
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-09-12 18:07:41 +0800

    Fix

--- database/2_create_table.sql
+++ database/2_create_table.sql
@@ -4,80 +4,82 @@
 -- Creates new tables.
 
 CREATE TABLE patients (
-  id NUMBER GENERATED AS IDENTITY,
-  nric CHAR(9) UNIQUE NOT NULL,
-  name VARCHAR(80) NOT NULL,
-  dob DATE NOT NULL,
-  gender NUMBER(1) NOT NULL,
-  phone CHAR(8) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  created_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  PRIMARY KEY (id)
+  user_name  VARCHAR(80),
+  nric       CHAR(9)      NOT NULL,
+  first_name VARCHAR(80)  NOT NULL,
+  last_name  VARCHAR(80)  NOT NULL,
+  dob        DATE         NOT NULL,
+  gender     NUMBER(1)    NOT NULL,
+  phone      CHAR(8)      NOT NULL,
+  address    VARCHAR(100) NOT NULL,
+  created_at DATE         DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATE         DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (user_name),
+  UNIQUE (nric)
 );
 
 CREATE TABLE staff (
-  id NUMBER GENERATED AS IDENTITY,
-  name VARCHAR(80) NOT NULL,
-  gender NUMBER(1) NOT NULL,
-  staff_type NUMBER(1) NOT NULL,
-  created_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  PRIMARY KEY (id)
+  user_name  VARCHAR(80),
+  first_name VARCHAR(80) NOT NULL,
+  gender     NUMBER(1)   NOT NULL,
+  staff_type NUMBER(1)   NOT NULL,
+  created_at DATE        DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATE        DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (user_name)
 );
 
 CREATE TABLE appointments (
-  id NUMBER GENERATED AS IDENTITY,
-  patient_id NUMBER NOT NULL,
-  doctor_id NUMBER NOT NULL,
-  receptionist_id NUMBER NOT NULL,
-  time DATE NOT NULL,
-  created_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  id                NUMBER      GENERATED AS IDENTITY,
+  patient_name      VARCHAR(80) NOT NULL,
+  doctor_name       VARCHAR(80) NOT NULL,
+  receptionist_name VARCHAR(80) NOT NULL,
+  time              DATE        NOT NULL,
+  created_at        DATE        DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at        DATE        DEFAULT CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (patient_id) REFERENCES patients(id),
-  FOREIGN KEY (doctor_id) REFERENCES staff(id),
-  FOREIGN KEY (receptionist_id) REFERENCES staff(id)
+  FOREIGN KEY (patient_name)      REFERENCES patients(user_name),
+  FOREIGN KEY (doctor_name)       REFERENCES staff(user_name),
+  FOREIGN KEY (receptionist_name) REFERENCES staff(user_name)
 );
 
 CREATE TABLE consultations (
-  id NUMBER GENERATED AS IDENTITY,
-  patient_id NUMBER NOT NULL,
-  doctor_id NUMBER NOT NULL,
-  receptionist_id NUMBER NOT NULL,
-  appointment_id NUMBER,
-  time_start DATE NOT NULL,
-  time_end DATE,
-  internal_notes VARCHAR(200),
-  created_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  id                NUMBER        GENERATED AS IDENTITY,
+  patient_name      VARCHAR(80)   NOT NULL,
+  doctor_name       VARCHAR(80)   NOT NULL,
+  receptionist_name VARCHAR(80)   NOT NULL,
+  appointment_id    NUMBER,
+  time_start        DATE          NOT NULL,
+  time_end          DATE,
+  internal_notes    VARCHAR(200),
+  created_at        DATE          DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at        DATE          DEFAULT CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (patient_id) REFERENCES patients(id),
-  FOREIGN KEY (doctor_id) REFERENCES staff(id),
-  FOREIGN KEY (receptionist_id) REFERENCES staff(id),
-  FOREIGN KEY (appointment_id) REFERENCES appointments(id)
+  FOREIGN KEY (patient_name)      REFERENCES patients(user_name),
+  FOREIGN KEY (doctor_name)       REFERENCES staff(user_name),
+  FOREIGN KEY (receptionist_name) REFERENCES staff(user_name),
+  FOREIGN KEY (appointment_id)    REFERENCES appointments(id)
 );
 
 CREATE TABLE payments (
-  id NUMBER GENERATED AS IDENTITY,
-  cashier_id NUMBER NOT NULL,
-  consultation_id NUMBER NOT NULL,
-  amount NUMBER(8,2) NOT NULL,
-  status NUMBER(1) DEFAULT 0 NOT NULL,
-  created_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  id              NUMBER        GENERATED AS IDENTITY,
+  cashier_name    NUMBER        NOT NULL,
+  consultation_id NUMBER        NOT NULL,
+  amount          NUMBER(8,2)   NOT NULL,
+  status          NUMBER(1)     DEFAULT 0 NOT NULL,
+  created_at      DATE          DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at      DATE          DEFAULT CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (cashier_id) REFERENCES staff(id),
+  FOREIGN KEY (cashier_name)    REFERENCES staff(user_name),
   FOREIGN KEY (consultation_id) REFERENCES consultations(id)
 );
 
 CREATE TABLE records (
-  id NUMBER GENERATED AS IDENTITY,
-  consultation_id NUMBER NOT NULL,
-  record_type NUMBER(2) NOT NULL,
-  results VARCHAR(200) NOT NULL,
-  created_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  id              NUMBER        GENERATED AS IDENTITY,
+  consultation_id NUMBER        NOT NULL,
+  record_type     NUMBER(2)     NOT NULL,
+  results         VARCHAR(200)  NOT NULL,
+  created_at      DATE          DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at      DATE          DEFAULT CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (consultation_id) REFERENCES consultations(id)
 );
--- database/3_create_policy.sql
+++ database/3_create_policy.sql
@@ -1,3 +1,15 @@
+-- Defines package.
+CREATE OR REPLACE package app_pkg is
+    procedure set_app_ctx is BEGIN
+        DBMS_SESSION.SET_CONTEXT('app_ctx', 'user_name', user);
+    end;
+end;
+
+-- Defines application context.
+CREATE OR REPLACE context app_ctx USING app_pkg;
+
+-- Defines trigger.
+
 -- Defines functions.
 
 -- Attaches policies.
