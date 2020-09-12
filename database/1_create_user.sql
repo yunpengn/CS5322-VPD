2251d7ddfac4d875dc516aa66c379d3a21f01f0f
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-09-12 17:31:50 +0800

    Fix

---
+++ database/1_create_user.sql
@@ -0,0 +1,7 @@
+-- Creates system user.
+CREATE USER app_admin IDENTIFIED BY not_admin;
+
+-- Grants necessary privileges.
+GRANT resource, connect TO app_admin;
+GRANT execute ON DBMS_RLS TO app_admin;
+GRANT SELECT ANY DICTIONARY to app_admin;
