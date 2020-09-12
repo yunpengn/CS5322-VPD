59e3b0f2bbd6220bbdeb3ca86e63c0f0c0ac7b31
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-09-13 02:05:14 +0800

    Fix

--- database/1_create_user.sql
+++ database/1_create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER app_admin IDENTIFIED BY not_admin;
 
 -- Grants necessary privileges.
-GRANT create session, create any context, connect, resource TO app_admin;
+GRANT create session, create any context, administer database trigger, connect, resource TO app_admin;
 GRANT execute ON DBMS_SESSION TO app_admin;
 GRANT execute ON DBMS_RLS TO app_admin;
 
