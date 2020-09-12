1c6687dcc3e442a90c348042a9e3236aa3d59cc2
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-09-12 19:54:28 +0800

    Add kill

--- database/0_cleanup.sql
+++ database/0_cleanup.sql
@@ -1,2 +1,6 @@
+-- Kills all active connections.
+SELECT SID, SERIAL# FROM V$SESSION WHERE USERNAME = 'APP_ADMIN';
+ALTER SYSTEM KILL SESSION '72, 60140';
+
 -- Drops existing user & records.
 DROP USER app_admin CASCADE;
