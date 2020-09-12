9aeac05412154aa7e43629a54fb9ba9913281a88
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-09-12 20:14:22 +0800

    Fix

--- database/3_create_policy.sql
+++ database/3_create_policy.sql
@@ -1,14 +1,25 @@
 -- Defines package.
 CREATE OR REPLACE package app_pkg is
-    procedure set_app_ctx is BEGIN
-        DBMS_SESSION.SET_CONTEXT('app_ctx', 'user_name', user);
-    end;
-end;
+    PROCEDURE set_app_ctx AS
+        user_name VARCHAR(80);
+        user_role VARCHAR(12);
+    BEGIN
+        user_name := SYS_CONTEXT('USERENV', 'SESSION_USER');
+        DBMS_SESSION.SET_CONTEXT('app_ctx', 'user_name', user_name);
+
+        SELECT STAFF_TYPE INTO user_role FROM STAFF WHERE USER_NAME = user_name;
+        DBMS_SESSION.SET_CONTEXT('app_ctx', 'user_role', user_role);
+    END;
+END;
 
 -- Defines application context.
 CREATE OR REPLACE context app_ctx USING app_pkg;
 
 -- Defines trigger.
+CREATE OR REPLACE TRIGGER logon_set_ctx AFTER LOGON ON DATABASE
+BEGIN
+    app_pkg.set_app_ctx();
+END;
 
 -- Defines functions.
 
