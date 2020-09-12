e0a2631c2f367b4ec03f97a039d1b10116e3db68
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-09-12 17:25:55 +0800

    Fix

--- database/cleanup.sql
+++ database/cleanup.sql
@@ -1,8 +1,8 @@
 -- Drops all existing tables.
 
+DROP TABLE records;
+DROP TABLE payments;
+DROP TABLE consultations;
+DROP TABLE appointments;
 DROP TABLE patients;
 DROP TABLE staff;
-DROP TABLE appointments;
-DROP TABLE consultations;
-DROP TABLE payments;
-DROP TABLE records;
