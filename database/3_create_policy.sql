5b9e1443cefe8c73f820382b542967e69bc884ec
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-09-19 15:43:53 +0800

    Add role-based policy

--- database/3_create_policy.sql
+++ database/3_create_policy.sql
@@ -11,8 +11,12 @@ CREATE OR REPLACE package body app_pkg is
         user_name := SYS_CONTEXT('userenv', 'SESSION_USER');
         DBMS_SESSION.SET_CONTEXT('app_ctx', 'user_name', user_name);
 
-        SELECT role_type INTO user_role FROM users WHERE USER_NAME = user_name;
-        DBMS_SESSION.SET_CONTEXT('app_ctx', 'user_role', user_role);
+        IF user_name = 'APP_ADMIN' THEN
+            DBMS_SESSION.SET_CONTEXT('app_ctx', 'user_role', 'admin');
+        ELSE
+            SELECT role_type INTO user_role FROM users WHERE USER_NAME = user_name;
+            DBMS_SESSION.SET_CONTEXT('app_ctx', 'user_role', user_role);
+        END IF;
     EXCEPTION
         WHEN NO_DATA_FOUND THEN NULL;
     END set_app_ctx;
@@ -31,7 +35,20 @@ END;
 CREATE OR REPLACE FUNCTION restrict_users(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
     RETURN VARCHAR2 AS cond VARCHAR2(100);
 BEGIN
-    cond := 'user_name = SYS_CONTEXT("app_ctx", "user_name")';
+    IF    SYS_CONTEXT('app_ctx', 'user_role') = 'admin' THEN
+        cond = '';
+    ELSIF SYS_CONTEXT('app_ctx', 'user_role') = 'patient' THEN
+        cond = '';
+    ELSIF SYS_CONTEXT('app_ctx', 'user_role') = 'doctor' THEN
+        cond = '';
+    ELSIF SYS_CONTEXT('app_ctx', 'user_role') = 'receptionist' THEN
+        cond = '';
+    ELSIF SYS_CONTEXT('app_ctx', 'user_role') = 'cashier' THEN
+        cond = '';
+    ELSE
+        cond = 'user_name = SYS_CONTEXT("app_ctx", "user_name")';
+    END IF;
+
     RETURN cond;
 END restrict_users;
 
@@ -42,5 +59,5 @@ BEGIN
         object_name     => 'users',
         policy_name     => 'policy_restrict_users',
         policy_function => 'restrict_users',
-        update_check    => true);
+        statement_types => 'select,insert,update,delete');
 END;
